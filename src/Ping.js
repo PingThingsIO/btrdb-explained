672bd660050e05e716b6244c1f012c27820dcaf4
Shaun Williams <shaun.e.williams@gmail.com>
2018-01-19 01:02:18 -0600

    finish mesh generation

--- src/Ping.js
+++ src/Ping.js
@@ -1,45 +1,5 @@
 import React, { Component } from "react";
-import delaunay from "delaunay-fast";
-
-function randomPoints(n, xmin, xmax, ymin, ymax, minDist) {
-  const points = [];
-  const xrange = xmax - xmin;
-  const yrange = ymax - ymin;
-  const randPoint = () => [
-    Math.random() * xrange + xmin,
-    Math.random() * yrange + ymin
-  ];
-  const dist = ([x0, y0], [x1, y1]) => {
-    const dx = x1 - x0;
-    const dy = y1 - y0;
-    return Math.sqrt(dx * dx + dy * dy);
-  };
-  const isPointValid = a => {
-    for (let b of points) {
-      if (dist(a, b) < minDist) return false;
-    }
-    return true;
-  };
-  const randValidPoint = () => {
-    while (true) {
-      const p = randPoint();
-      if (isPointValid(p)) return p;
-    }
-  };
-  for (let i = 0; i < n; i++) {
-    points.push(randValidPoint());
-  }
-  return points;
-}
-
-function triangulate(points) {
-  const indices = delaunay.triangulate(points);
-  const triangles = [];
-  for (let i = 0; i < indices.length; i += 3) {
-    triangles.push(indices.slice(i, i + 3));
-  }
-  return triangles;
-}
+import { pointLookup, randomMesh } from "./geometry";
 
 class Ping extends Component {
   constructor(props) {
@@ -49,7 +9,9 @@ class Ping extends Component {
       width: 1024,
       height: 600
     };
-    this.state.points = this.randomPoints();
+    const { points, triangles } = this.randomMesh();
+    this.state.points = points;
+    this.state.triangles = triangles;
   }
   componentWillMount() {
     this.computeDerivedState(this.props, this.state);
@@ -59,31 +21,31 @@ class Ping extends Component {
   }
   computeDerivedState = (props, state) => {
     const pixelRatio = window.devicePixelRatio || 1;
-    const triangles = triangulate(state.points);
-
     this.ds = {
-      pixelRatio,
-      triangles
+      pixelRatio
     };
   };
-  randomPoints = () => {
+  randomMesh = () => {
     const { width, height } = this.state;
-    const points = randomPoints(12, 0, width, 0, height, 60);
-    return points;
+    return randomMesh({
+      n: 18,
+      xdomain: [0, width],
+      ydomain: [0, height],
+      minDist: 60,
+      minArea: Math.pow(120, 2) / 2,
+      minAngle: 20 * Math.PI / 180
+    });
   };
   drawTriangles = ctx => {
-    const { points } = this.state;
-    const { triangles } = this.ds;
+    const { points, triangles } = this.state;
 
     ctx.strokeStyle = "#789";
-    for (let [a, b, c] of triangles) {
-      const pa = points[a];
-      const pb = points[b];
-      const pc = points[c];
+    for (let indexes of triangles) {
+      const [a, b, c] = pointLookup(points, indexes);
       ctx.beginPath();
-      ctx.moveTo(pa[0], pa[1]);
-      ctx.lineTo(pb[0], pb[1]);
-      ctx.lineTo(pc[0], pc[1]);
+      ctx.moveTo(a[0], a[1]);
+      ctx.lineTo(b[0], b[1]);
+      ctx.lineTo(c[0], c[1]);
       ctx.closePath();
       ctx.stroke();
     }
---
+++ src/geometry.js
@@ -0,0 +1,101 @@
+import delaunay from "delaunay-fast";
+
+// return triplets of indices
+function triangulate(points) {
+  const indices = delaunay.triangulate(points);
+  const triangles = [];
+  for (let i = 0; i < indices.length; i += 3) {
+    triangles.push(indices.slice(i, i + 3));
+  }
+  return triangles;
+}
+
+function pointLookup(points, indexes) {
+  return indexes.map(i => points[i]);
+}
+
+function dist([x0, y0], [x1, y1]) {
+  const dx = x1 - x0;
+  const dy = y1 - y0;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function triangleArea([p0, p1, p2]) {
+  // from: https://en.wikipedia.org/wiki/Heron%27s_formula
+  const a = dist(p0, p1);
+  const b = dist(p1, p2);
+  const c = dist(p2, p0);
+  const s = (a + b + c) / 2;
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+function triangleAngles([p0, p1, p2]) {
+  // from: https://en.wikipedia.org/wiki/Law_of_cosines
+  const a = dist(p0, p1);
+  const b = dist(p1, p2);
+  const c = dist(p2, p0);
+  const angle = (a, b, c) => Math.acos((c * c - a * a - b * b) / (-2 * a * b));
+  return [angle(a, b, c), angle(b, c, a), angle(c, a, b)];
+}
+
+function randomMesh({ n, xdomain, ydomain, minDist, minArea, minAngle }) {
+  const points = [];
+  const [xmin, xmax] = xdomain;
+  const [ymin, ymax] = ydomain;
+  const xrange = xmax - xmin;
+  const yrange = ymax - ymin;
+  const randPoint = () => [
+    Math.random() * xrange + xmin,
+    Math.random() * yrange + ymin
+  ];
+  const isPointValid = a => {
+    // check dist
+    for (let b of points) {
+      if (dist(a, b) < minDist) return false;
+    }
+
+    points.push(a);
+    let valid = true;
+    for (let indexes of triangulate(points)) {
+      const tri = pointLookup(points, indexes);
+      // check area
+      if (triangleArea(tri) < minArea) {
+        valid = false;
+      } else {
+        // check angles
+        for (let a of triangleAngles(tri)) {
+          if (a < minAngle) valid = false;
+        }
+      }
+      if (!valid) break;
+    }
+    points.pop();
+    return valid;
+  };
+  const randValidPoint = () => {
+    const maxTries = 100;
+    for (let i = 0; i < maxTries; i++) {
+      const p = randPoint();
+      if (isPointValid(p)) {
+        // console.log(`${i} tries`);
+        return p;
+      }
+    }
+    // console.error(`could not find valid point in ${maxTries} tries`);
+    return null;
+  };
+  for (let i = 0; i < n; i++) {
+    const p = randValidPoint();
+    if (p) {
+      points.push(p);
+    } else {
+      // restart search since we ended up at a dead-end
+      return randomMesh({ n, xdomain, ydomain, minDist, minArea, minAngle });
+    }
+  }
+  const triangles = triangulate(points);
+
+  return { points, triangles };
+}
+
+export { pointLookup, randomMesh };
